a7359aafac06ee8f0c3e59ab38c93d730c8e09a5
Andrew D'Ambrosio <adambrosio20@gmail.com>
2020-04-20 15:54:50 -0400

    updated JS

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # homework-6
+// Input field for city (Bootstrap)
+// Div that will populate with the current forecast for input city
+    // Divs inside that will house various bits of information
+// 5 Cards will populate underneath that will update with the 5 day forecast (seperate API)
+// Need moment.js
--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,46 @@
 // Input field for city (Bootstrap)
 // Div that will populate with the current forecast for input city
+    // Divs inside that will house various bits of information
 // 5 Cards will populate underneath that will update with the 5 day forecast (seperate API)
-    // Both will need moment.js
-
+// Need moment.js
 
 var cityID = "houston";
 var APIKey = "&appid=3f2c167c8daff8f6c0523b5e972f1c83"
 
 var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityID + APIKey;
 // AJAX call for current forecast
+
+function initPage () {
 $.ajax({
     url: queryURL,
     method:'GET'
 }).then(function(response) {
     console.log(response);
+// Variables being defined for data retrieved from the response object
+    var cityName = response.name;
+    var currentDate = moment().format("MMMM Do" + ", " + "YYYY");
+    var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+    var humidity = response.main.humidity;
+    var windSpeed = response.wind.speed + " MPH";
+    var uvIndex = 
+    console.log(cityName);
+    console.log(currentDate);
+    console.log(tempF);
+    console.log(humidity);
+    console.log(windSpeed);
+    
+    $(".city-id").text(cityName + " " + currentDate);
+    $(".temp").text(tempF);
+    $(".humidity").text(humidity);
+    $(".wind").text(windSpeed);
+    
 });
 
 
+
+}
+
+initPage ();
 
 
 
@@ -26,10 +50,10 @@ $.ajax({
 // // Var for city
 // var city = "austin";
 // // queryURL defined for 5 day forecast
-// var queryURL = "https://api.openweathermap.org/data/2.5/forecast?id=" + city + "&appid=3f2c167c8daff8f6c0523b5e972f1c83";
+// var query = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + APIKey;
 // // AJAX call
 // $.ajax({
-//     url: queryURL,
+//     url: query,
 //     method:'GET'
 // }).then(function(response){
 //     console.log(response);
